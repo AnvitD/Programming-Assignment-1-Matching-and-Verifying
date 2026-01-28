91dea61651d2e88da37f51ee8efa097e8aed8ad5
KalimsCodingWonderland <159584563+KalimsCodingWonderland@users.noreply.github.com>
2026-01-28 18:49:22 -0500

    Added function definition + quick test case to test functionality

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-while free_hos:
+def gale_shapley(n, hos_prefs, stu_prefs):
+    hos_prefs = [None] + hos_prefs
+    stu_prefs = [None] + stu_prefs
+
+    hos_matches = [None] * (n + 1)
+    stu_matches = [None] * (n + 1)
+    free_hos = list(range(1, n + 1))
+    next_proposal_index = [0] * (n + 1)
+        
+        while free_hos:
         hos = free_hos[0]
         stu = hos_prefs[hos][next_proposal_index[hos]]
         next_proposal_index[hos] += 1
@@ -17,3 +26,12 @@ while free_hos:
                 free_hos.append(curr)
 
     return hos_matches[1:], stu_matches[1:]
+
+print(gale_shapley( 3,
+    [[1,2,3], 
+     [2,3,1], 
+     [2,1,3]], 
+    [[2,1,3],  
+     [1,2,3],  
+     [1,2,3]]
+))  
